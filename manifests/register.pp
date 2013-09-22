58e9ef1fddd15e6eda6e1c19c21e62f7dcf4b0ea
Andrew Leonard <andy@hurricane-ridge.com>
2013-09-22 07:44:01 -0700

    Add define to register /etc/rc.local fragements.

---
+++ manifests/register.pp
@@ -0,0 +1,45 @@
+# == Define: rclocal::register
+#
+# Register a fragment to be placed in /etc/rc.local.
+#
+# === Parameters
+#
+# [*content*]
+#   Text of /etc/rc.local fragment.
+#
+# [*priority*]
+#   Priority of the /etc/rc.local fragment.
+#
+# [*source*]
+#   File containing the contents of the /etc/rc.local fragment.  If empty,
+#   'content' parameter is used instead.
+#
+# === Examples
+#
+#
+#
+# === Authors
+#
+# Andrew Leonard
+#
+# === Copyright
+#
+# Copyright 2013 Andrew Leonard
+#
+define rclocal::register($content='', $priority=50, $source=false) {
+
+  if $source {
+    concat::fragment{ "rclocal_fragment_${name}":
+      target => '/etc/rc.local',
+      order  => $priority,
+      source => $source,
+    }
+  } else {
+    concat::fragment{ "rclocal_fragment_${name}":
+      target  => '/etc/rc.local',
+      content => $content,
+      order   => $priority,
+    }
+  }
+
+}
