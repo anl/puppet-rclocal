b877d2984cea2c3dea810e4551849337c7cd60ad
Andrew Leonard <andy@hurricane-ridge.com>
2013-09-22 06:54:09 -0700

    Initial working implementation - noop other than permission change.

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,5 +37,21 @@
 #
 class rclocal {
 
+  concat { '/etc/rc.local':
+    owner => 'root',
+    group => 'root',
+    mode  => '0555', # correct?
+  }
 
+  concat::fragment { 'rc.local_header':
+    target => '/etc/rc.local',
+    order  => 01,
+    source => 'puppet:///modules/rclocal/header'
+  }
+
+  concat::fragment { 'rc.local_footer':
+    target => '/etc/rc.local',
+    order  => 99,
+    source => 'puppet:///modules/rclocal/footer'
+  }
 }
